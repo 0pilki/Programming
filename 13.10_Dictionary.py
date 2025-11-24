4e5432a295f5886104752f2a2154fcd91dc1a7a8
0pilki <72854970+0pilki@users.noreply.github.com>
2025-11-24 13:23:18 +0100

    Add files via upload

---
+++ 13.10_Dictionary.py
@@ -0,0 +1,18 @@
+city = {
+    "Name":"Lübeck",
+    "Postal Codes":"23501-23570",
+    "State":"Schleiswig-Holstein",
+    "Total population":217.061,
+    "Total area in km2": 214.13
+}
+
+NewCity = city["State"].title()
+print(f"Bad Schwartau situated in {NewCity} as well.")
+
+usin = input()
+mistake = city.get (f"{usin}", "Dolbaeb? Doesn't exist!") #get "Name", or print "Dolbaeb?".
+print(mistake)
+if 
+
+for key, value in city.items():
+    print(f"{key} : {value}")
